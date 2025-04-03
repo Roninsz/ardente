07d77314a9fe1e3278b247d7bc4524ad6621b747
Roninsz <mariorodrigues.enzo@gmail.com>
2025-04-03 19:31:53 -0300

    magnetism

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './styles/App.css'
 import Home from './pages/Home.jsx'
 import Contact from './pages/Contact.jsx'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,47 @@
 import '../styles/hero.css'
 import seta from '../assets/seta.png'
+import logoBG from '../assets/logoBackground.png'
+import { useEffect, useRef } from 'react';
 
 export default function Hero() {
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            const image = imageRef.current;
+            if (!image) return;
+
+            const rect = image.getBoundingClientRect();
+            const mouseX = e.clientX - rect.left - rect.width / 2;
+            const mouseY = e.clientY - rect.top - rect.height / 2;
+
+            const moveX = -mouseX * 0.05; // Ajuste a intensidade do efeito
+            const moveY = -mouseY * 0.05;
+
+            image.style.transform = `translate(${moveX}px, ${moveY}px)`;
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+        return () => window.removeEventListener('mousemove', handleMouseMove);
+    }, []);
+
     return(
         <div className="hero-section">
-            <h1>Identidades de Marca para negócios<br/>
-            <span>Inovadores e tecnológicos</span> </h1>
+            <div className="hero-background">
+                <img ref={imageRef} src={logoBG} alt="Background Logo" />
+            </div>
+            <h1 className="slide-in-reverse">
+                <span id="sp1">Identidades de Marca para negócios</span><br/>
+                <span>Inovadores e tecnológicos</span>
+            </h1>
             <div id="hero" className="CTA-conteiner">
                 <img src={seta} alt=""/>
-                <a href="#destaques">
-                    FORJE<br/>
-                    SUA<br/>
-                    MARCA
+                <a href="#destaques" className="slide-in">
+                    <span>FORJE</span><br/>
+                    <span>SUA</span><br/>
+                    <span>MARCA</span>
                 </a>
             </div>
         </div>
-    )
+    );
 }
